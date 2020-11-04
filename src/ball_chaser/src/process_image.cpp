df8dc7e2d88ab26693f639cd4442c4afec22ae87
Ulrich Pech <ulrichpech@gmail.com>
2020-11-04 21:59:44 +0100

    refactor

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -7,7 +7,7 @@ ros::ServiceClient client;
 
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z) {
-    ROS_INFO_STREAM("Moving the bot to the ball");
+    //ROS_INFO_STREAM("Moving the bot to the ball");
 
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
@@ -17,34 +17,55 @@ void drive_robot(float lin_x, float ang_z) {
         ROS_ERROR("Failed to call service ball_chaser");
 }
 
+int identify_side(int pixel_idx, int step) {
+    int ret = 0;// stop
+    int pos = (pixel_idx % step) / 3;
+    int width = step / 3;
+    if (pos < width / 3) {
+        ret = 1;// left
+    } else if (pos > 2 * width / 3) {
+        ret = 2;// right
+    } else if (pos >= width / 3 && pos <= 2 * width / 3) {
+        ret = 3;// center
+    }
+
+    return ret;
+}
+
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img) {
+
     int white_pixel = 255;
     float lin_x = 0.0;
     float ang_z = 0.0;
-    int height = -1;
-    int width = -1;
     int stop = img.height * img.step - 3;
+    int side = 0;
+
     // Loop through each pixel in the image and check if there's a bright white one
-    for (int i = 0; i < stop; i+=3) {
-        if (img.data[i] == white_pixel && img.data[i + 1] == white_pixel && img.data[i + 2] == white_pixel ) {
-            height = i / img.step;
-            width = i % img.step;
+    for (int i = 0; i < stop; i += 3) {
+        if (img.data[i] == white_pixel && img.data[i + 1] == white_pixel && img.data[i + 2] == white_pixel) {
+            side = identify_side(i, img.step);
             break;
         }
     }
 
+
+
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
-    if (width <= img.step * 0.3 && width >= 0) { // left
-        ang_z = 0.5;
-    } else if (width > img.step * 0.7 && width <= img.step) { // right
-        ang_z = -0.5;
-    } else if (width != -1) { // center
-        lin_x = 0.5;
-    }
     // Request a stop when there's no white ball seen by the camera
-
+    switch (side) {
+        case 0:
+            break;
+        case 1:
+            ang_z = 0.5;
+            break;
+        case 2:
+            ang_z = -0.5;
+            break;
+        case 3:
+            lin_x = 0.5;
+    }
     drive_robot(lin_x, ang_z); // drive the bot
 }
 
